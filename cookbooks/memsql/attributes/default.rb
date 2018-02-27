5447f525bf686392fc2f13de1bc365734c8c53ea
503003625 <attila.kopjas@ge.com>
2018-02-27 17:32:34 +0100

    first

--- cookbooks/memsql/attributes/default.rb
+++ cookbooks/memsql/attributes/default.rb
@@ -1,6 +1,17 @@
-default['memsql']['version'] = '4.1.9'
+default['memsql']['base-s3-path'] = 'https://s3.eu-central-1.amazonaws.com/memsql-training'
+
+default['memsql']['version'] = '6.0.7'
+default['memsql']['release'] = "#{default['memsql']['version']}-1.el7.ngx.x86_64"
+default['memsql']['file-name'] = "nginx-#{default['memsql']['nginx-release']}.rpm"
+default['memsql']['s3-path'] = "#{default['memsql']['s3-path']}/#{default['memsql']['file-name']}"
+
+
+
+default['memsql']['version'] = '6.0.7'
 default['memsql']['download_url'] = "http://download.memsql.com/memsql-ops-#{node['memsql']['version']}/memsql-ops-#{node['memsql']['version']}.tar.gz"
 default['memsql']['download_dir'] = Chef::Config[:file_cache_path]
 default['memsql']['checksum'] = 'f6bdf8451ce4d8cd79a555accbcbbef09eb828e0c4c12d1c632442a82739466f'
 default['memsql']['bin_download_url'] = 'http://download.memsql.com/releases/latest/memsqlbin_amd64.tar.gz'
 default['memsql']['bin_checksum'] = 'c900e586c3f8f3fe52a59a9872a70e0b03d98df3ed7cc9a87229947b7a3e4ab0'
+
+ 	
--- cookbooks/memsql/recipes/master_install.rb
+++ cookbooks/memsql/recipes/master_install.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe 's3_file'
 
 memsql_version = node['memsql']['version']
 memsql_download_url = node['memsql']['download_url']
@@ -23,6 +24,23 @@ memsql_download_dir = node['memsql']['download_dir']
 memsql_checksum = node['memsql']['checksum']
 memsql_bin_download_url = node['memsql']['bin_download_url']
 memsql_bin_checksum = node['memsql']['bin_checksum']
+
+
+directory '/tmp/aen' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  #action :nothing
+  #notifies :delete, 'directory[/tmp/aen]' # at the end of the recipe
+end
+
+s3_file "/tmp/aen/#{node['aen']['aen-gateway-file-name']}" do
+  remote_path node['aen']['aen-gateway-s3-path']
+  mode '0744'
+  #action :nothing
+end
+
 
 remote_file "#{memsql_download_dir}/memsql-ops-#{memsql_version}.tar.gz" do
   source memsql_download_url
