cfbed356b3f566c3398bb061f89157d52d7f9b3c
503003625 <attila.kopjas@ge.com>
2018-02-28 17:40:20 +0100

    first

--- memsql/metadata.rb
+++ memsql/metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Jonathan Klinginsmith'
 maintainer_email 'jklingin@indiana.edu'
 license 'Apache 2.0'
 description 'Installs/Configures MemSQL'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-depends 's3_file', '~> 2.8.5'
+#depends 's3_file', '~> 2.8.5'
